d80f171d53e9d7e9e1ddb234cfd628dee3444475
2552895864 <2552895864@qq.com>
2020-02-20 16:24:14 +0800

    增加了库存记录和出库记录页面

--- config/config.js
+++ config/config.js
@@ -117,12 +117,18 @@ export default {
               path: '/list',
               component: './ListTableList',
             },
-            // {
-            //   name: 'list.cargo-list',
-            //   icon: 'table',
-            //   path: '/cargolist',
-            //   component: './CargoList',
-            // },
+            {
+              name: 'list.cargo-list',
+              icon: 'table',
+              path: '/cargolist',
+              component: './CargoList',
+            },
+            {
+              name: 'list.out-cargo-list',
+              icon: 'table',
+              path: '/outcargolist',
+              component: './OutCargoList',
+            },
             {
               component: './404',
             },
