e062c017f253bce4558ddf9df0127492b8c585fb
范轶洁 <fanyjb@digitalchina.com>
2020-02-21 12:45:20 +0800

    remove other login option

--- src/layouts/UserLayout.jsx
+++ src/layouts/UserLayout.jsx
@@ -1,11 +1,11 @@
-import { DefaultFooter, getMenuData, getPageTitle } from '@ant-design/pro-layout';
+import { getMenuData, getPageTitle } from '@ant-design/pro-layout';
 import { Helmet } from 'react-helmet';
 import { Link } from 'umi';
 import React from 'react';
 import { formatMessage } from 'umi-plugin-react/locale';
 import { connect } from 'dva';
 import SelectLang from '@/components/SelectLang';
-import logo from '../assets/logo.svg';
+import logo from '../assets/sinotrans-logo.png';
 import styles from './UserLayout.less';
 
 const UserLayout = props => {
@@ -44,14 +44,12 @@ const UserLayout = props => {
             <div className={styles.header}>
               <Link to="/">
                 <img alt="logo" className={styles.logo} src={logo} />
-                <span className={styles.title}>Ant Design</span>
               </Link>
             </div>
-            <div className={styles.desc}>Ant Design 是西湖区最具影响力的 Web 设计规范</div>
+            <div className={styles.desc}>中外运股份有限公司</div>
           </div>
           {children}
         </div>
-        <DefaultFooter />
       </div>
     </>
   );
