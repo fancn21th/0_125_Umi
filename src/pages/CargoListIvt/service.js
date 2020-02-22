ac32ac9ae47e42c5a992059d95a033c2d8a6e783
2552895864 <2552895864@qq.com>
2020-02-22 18:44:23 +0800

    real-api test

--- src/pages/CargoListIvt/service.js
+++ src/pages/CargoListIvt/service.js
@@ -1,17 +1,25 @@
 import request from '@/utils/request';
 import { ApiTransformToData } from '../../utils/api-to-data-cargo';
 
-const defaultNo = '30000214-002';
+//测试用默认盘点编号
+// const defaultNo = '30000214-002';
 
-export async function queryCargoListIvt({ current, pageSize, sorter, inventoryno = defaultNo }) {
-  const data = await request('/api/sinoapi/getcargolistivt', {
-    params: {
-      pageindex: current - 1,
-      pageSize,
-      inventoryno,
-    },
+export async function queryCargoListIvt({ current, pageSize, sorter, inventoryno }) {
+  if (inventoryno) {
+    const data = await request('/api/sinoapi/getcargolistivt', {
+      params: {
+        pageindex: current - 1,
+        pageSize,
+        inventoryno,
+      },
+    });
+    return ApiTransformToData(data);
+  }
+  return Promise.resolve({
+    total: 0,
+    success: true,
+    data: [],
   });
-  return ApiTransformToData(data);
 }
 
 export async function queryIvtList({ pageindex, pagesize }) {
