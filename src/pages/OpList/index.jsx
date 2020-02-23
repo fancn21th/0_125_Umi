4ccc821e3b624f9247eddf26569fa0fe6e7cfddb
2552895864 <2552895864@qq.com>
2020-02-23 14:01:48 +0800

    日志弹窗、功能按钮

--- src/pages/OpList/index.jsx
+++ src/pages/OpList/index.jsx
@@ -1,11 +1,16 @@
-import { DownOutlined, PlusOutlined } from '@ant-design/icons';
-import { Button, Divider, Dropdown, Menu, message, Input, Typography, Modal } from 'antd';
+import { Button, Divider, Dropdown, Menu, message, Input, Typography } from 'antd';
 import React, { useState, useRef } from 'react';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import ProTable, { TableDropdown } from '@ant-design/pro-table';
-import { queryCargos } from './service';
-import { columnFunc } from '../../config/col-config-oplist';
-const columns = columnFunc(TableDropdown);
+import LogModal from './components/LogModal';
+import {
+  queryOpList,
+  queryUploadResultByInorder,
+  queryUploadResult,
+  updateStatus,
+} from './service';
+import { columns } from '../../config/col-config-oplist';
+import { columns as logColumns } from '../../config/col-config-loglist';
 const { Search } = Input;
 const { Text } = Typography;
 
@@ -21,7 +26,7 @@ const TableList = () => {
   const [opTableparams, setOpTableparams] = useState({
     sorter,
   });
-  const [logTableparams, setLogTableparams] = useState({});
+  const [logData, setLogData] = useState({});
   const [logModalVisibility, setLogModalVisibility] = useState(false);
 
   const actionRef = useRef();
@@ -75,9 +80,20 @@ const TableList = () => {
           </Button>,
           <Button
             type="primary"
-            onClick={() => {
+            onClick={async e => {
               if (validate(orderno)) {
-                setLogModalVisibility(true);
+                const hide = message.loading('正在查询...');
+                try {
+                  const data = await queryUploadResultByInorder(orderno);
+                  hide();
+                  message.success('查询成功');
+                  await setLogData(data);
+                  return setLogModalVisibility(true);
+                } catch (error) {
+                  hide();
+                  message.error(`查询失败,原因：${error.message}`);
+                  return setLogModalVisibility(false);
+                }
               } else {
                 message.warning('请输入合规单号，示例前缀：IN、WV、OU');
               }
@@ -96,21 +112,66 @@ const TableList = () => {
             style={{ width: 200 }}
           />,
         ]}
-        request={params => queryCargos(params)}
-        columns={columns}
+        request={params => queryOpList(params)}
+        columns={[
+          ...columns,
+          {
+            title: '日志',
+            dataIndex: 'option',
+            valueType: 'option',
+            render: (text, row) => [
+              <a
+                onClick={async () => {
+                  const hide = message.loading('正在查询...');
+                  try {
+                    const { OpSN: id } = row;
+                    const data = await queryUploadResult(id);
+                    hide();
+                    message.success('查询成功');
+                    await setLogData(data);
+                    return setLogModalVisibility(true);
+                  } catch (error) {
+                    hide();
+                    message.error(`查询失败,原因：${error.message}`);
+                    return setLogModalVisibility(false);
+                  }
+                }}
+              >
+                查询日志
+              </a>,
+              <TableDropdown
+                onSelect={async key => {
+                  const status = {
+                    updatefalse: 0,
+                    updatetrue: 1,
+                  };
+                  const hide = message.loading('正在同步...');
+                  try {
+                    const { OpSN: id } = row;
+                    await updateStatus(id, status[key]);
+                    hide();
+                    message.success('同步成功');
+                  } catch (error) {
+                    hide();
+                    message.error('同步失败');
+                  }
+                }}
+                menus={[
+                  { key: 'updatefalse', name: '强制同步' },
+                  { key: 'updatetrue', name: '同步成功' },
+                ]}
+              />,
+            ],
+          },
+        ]}
       />
-      <Modal
-        visible={logModalVisibility}
+      <LogModal
+        columns={logColumns}
+        logData={logData}
+        modalVisible={logModalVisibility}
         title="上传日志"
         onCancel={() => setLogModalVisibility(false)}
-        footer={[
-          <Button key="back" onClick={() => setLogModalVisibility(false)}>
-            取消
-          </Button>,
-        ]}
-      >
-        test
-      </Modal>
+      ></LogModal>
     </PageHeaderWrapper>
   );
 };
--- src/pages/OpList/service.js
+++ src/pages/OpList/service.js
@@ -1,7 +1,8 @@
 import request from '@/utils/request';
 import { ApiTransformToData } from '../../utils/api-to-data-op';
+import { ApiTransformToData as ApiTransformToLogData } from '../../utils/api-to-data-loglist';
 
-export async function queryCargos({ current, pageSize, sorter, OrderNo }) {
+export async function queryOpList({ current, pageSize, sorter, OrderNo }) {
   const data = await request('/api/sinoapi/getoplist', {
     params: {
       pageindex: current - 1,
@@ -10,4 +11,31 @@ export async function queryCargos({ current, pageSize, sorter, OrderNo }) {
     },
   });
   return ApiTransformToData(data);
+}
+
+export async function queryUploadResultByInorder(inorder) {
+  const data = await request('/api/sinoapi/getuploadresultbyinorder', {
+    params: {
+      inorder,
+    },
+  });
+  return ApiTransformToLogData(data);
+}
+
+export async function queryUploadResult(opsn) {
+  const data = await request('/api/sinoapi/getuploadresult', {
+    params: {
+      opsn,
+    },
+  });
+  return ApiTransformToLogData(data);
+}
+
+export async function updateStatus(opsn, status) {
+  return request('/api/sinoapi/updatesyncstatus', {
+    params: {
+      opsn,
+      status,
+    },
+  });
 }
