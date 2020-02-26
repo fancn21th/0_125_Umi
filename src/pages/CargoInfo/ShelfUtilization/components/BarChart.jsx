484a5e9e91794bab7b0c115168288ab348bf1e38
范轶洁 <fanyjb@digitalchina.com>
2020-02-26 15:45:05 +0800

    char integration

--- src/pages/CargoInfo/ShelfUtilization/components/BarChart.jsx
+++ src/pages/CargoInfo/ShelfUtilization/components/BarChart.jsx
@@ -1,76 +1,40 @@
 import React from 'react';
 import {
-  G2,
+  // G2,
   Chart,
   Geom,
   Axis,
   Tooltip,
-  Coord,
-  Label,
-  Legend,
-  View,
-  Guide,
-  Shape,
-  Facet,
-  Util,
+  // Coord,
+  // Label,
+  // Legend,
+  // View,
+  // Guide,
+  // Shape,
+  // Facet,
+  // Util,
 } from 'bizcharts';
 
-class Basiccolumn extends React.Component {
-  render() {
-    const data = [
-      {
-        year: '1951 年',
-        sales: 38,
-      },
-      {
-        year: '1952 年',
-        sales: 52,
-      },
-      {
-        year: '1956 年',
-        sales: 61,
-      },
-      {
-        year: '1957 年',
-        sales: 145,
-      },
-      {
-        year: '1958 年',
-        sales: 48,
-      },
-      {
-        year: '1959 年',
-        sales: 38,
-      },
-      {
-        year: '1960 年',
-        sales: 38,
-      },
-      {
-        year: '1962 年',
-        sales: 38,
-      },
-    ];
-    const cols = {
-      sales: {
-        tickInterval: 20,
-      },
-    };
-    return (
-      <div>
-        <Chart height={400} data={data} scale={cols} forceFit>
-          <Axis name="year" />
-          <Axis name="sales" />
-          <Tooltip
-            crosshairs={{
-              type: 'y',
-            }}
-          />
-          <Geom type="interval" position="year*sales" />
-        </Chart>
-      </div>
-    );
-  }
-}
+const BarChart = ({ data }) => {
+  // const { data } = props;
+  const cols = {
+    Time: { alias: '日期' },
+    Cp: { alias: '空闲率' },
+  };
+  return (
+    <div>
+      <Chart height={400} data={data} scale={cols} forceFit>
+        <Axis name="Time" />
+        <Axis name="Cp" />
+        <Tooltip
+          crosshairs={{
+            type: 'y',
+          }}
+        />
+        <Geom type="interval" position="Time*Cp" />
+      </Chart>
+    </div>
+  );
+};
 
-export default Basiccolumn;
+export default BarChart;
--- src/pages/CargoInfo/ShelfUtilization/index.jsx
+++ src/pages/CargoInfo/ShelfUtilization/index.jsx
@@ -1,11 +1,26 @@
-import { DatePicker, Button, Card } from 'antd';
 import React from 'react';
+import { DatePicker, Button, Card } from 'antd';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
+import { connect } from 'dva';
 import BarChart from './components/BarChart';
 
 const { RangePicker } = DatePicker;
 
-const ShelfUtilization = () => {
+const ShelfUtilization = props => {
+  const { dispatch, data } = props;
+
+  const onSearch = () => {
+    if (dispatch) {
+      dispatch({
+        type: 'shelfUtilization/fetchChartData',
+        payload: {
+          begin: '2020-01-01',
+          end: '2020-01-01',
+        },
+      });
+    }
+  };
+
   const mainSearch = (
     <div
       style={{
@@ -13,16 +28,18 @@ const ShelfUtilization = () => {
       }}
     >
       <RangePicker format="YYYY-MM-DD" placeholder={['开始日期', '结束日期']} />
-      <Button type="primary">搜索</Button>
+      <Button type="primary" onClick={onSearch}>
+        搜索
+      </Button>
     </div>
   );
   return (
     <PageHeaderWrapper content={mainSearch}>
       <Card bordered={false}>
-        <BarChart />
+        <BarChart data={data} />
       </Card>
     </PageHeaderWrapper>
   );
 };
 
-export default ShelfUtilization;
+export default connect(({ shelfUtilization: { data } }) => ({ data }))(ShelfUtilization);
