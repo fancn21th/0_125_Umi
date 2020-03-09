7f0feb98293788ad192b24a12de72ff984d2f772
范轶洁 <fanyjb@digitalchina.com>
2020-03-09 16:51:37 +0800

    remove link in breadcrumb

--- src/layouts/BasicLayout.jsx
+++ src/layouts/BasicLayout.jsx
@@ -136,14 +136,15 @@ const BasicLayout = props => {
         // },
         ...routers,
       ]}
-      itemRender={(route, params, routes, paths) => {
-        const first = routes.indexOf(route) === 0;
-        return first ? (
-          <Link to={paths.join('/')}>{route.breadcrumbName}</Link>
-        ) : (
-          <span>{route.breadcrumbName}</span>
-        );
-      }}
+      itemRender={(route, params, routes, paths) => (
+        // const first = routes.indexOf(route) === 0;
+        // return first ? (
+        //   <Link to={paths.join('/')}>{route.breadcrumbName}</Link>
+        // ) : (
+        //   <span>{route.breadcrumbName}</span>
+        // );
+        <span>{route.breadcrumbName}</span>
+      )}
       footerRender={footerRender}
       menuDataRender={menuDataRender}
       rightContentRender={() => <RightContent />}
