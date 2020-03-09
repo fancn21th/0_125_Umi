bf86cc0df3a51e1210aeced30aae8a8367bfa8f7
2552895864 <2552895864@qq.com>
2020-03-09 17:35:04 +0800

    部分输入框宽度调整

--- src/pages/Order/OrderGoods/index.jsx
+++ src/pages/Order/OrderGoods/index.jsx
@@ -28,6 +28,7 @@ const TableList = () => {
     <div className="dc-headerContent-wrapper">
       <Text>单号：</Text>
       <Input
+        className="dc-orderno-input-width"
         placeholder="请输入单号"
         value={orderNoValue}
         onChange={e => {
